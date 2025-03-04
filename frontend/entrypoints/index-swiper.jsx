b9f3982223a1e20ea1496e154c9648c47e5b16ac
xingyanpo <xingyanpo@gmail.com>
2025-03-04 11:11:10 +0800

    change react set

--- frontend/entrypoints/index-swiper.jsx
+++ frontend/entrypoints/index-swiper.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import style from './index-swiper.module.scss'
 
 function App() {
+  const [light, setLight]= React.useState(false);
   return (
-    <div className={`${style.text} container mx-auto my-12 md:my-24 bg-blue-50 `}>App</div>
+    <div className={`${style['text-light']} container mx-auto my-12 md:my-24 bg-blue-50 `}>
+      <button onClick={() => setLight(!light)}>{light ? '开灯': '关灯'}</button>
+    </div>
   )
 }
 
