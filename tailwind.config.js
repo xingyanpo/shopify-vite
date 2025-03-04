a4975bd62d0a8e19a398211747b0dbaa9dbd0f2a
xingyanpo <xingyanpo@gmail.com>
2025-03-04 09:23:40 +0800

    upgrade react and tailwind

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./frontend/**/*.js', './frontend/**/*.jsx', './**/*.liquid'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import shopify from 'vite-plugin-shopify';
 import react from '@vitejs/plugin-react';
 import cleanup from '@by-association-only/vite-plugin-shopify-clean';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     cleanup(),
     shopify(),
     react(),
+    tailwindcss(),
   ],
   build: {
     emptyOutDir: false
